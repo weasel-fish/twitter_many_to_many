ada9bb20dbc18989a0ba37ab611747111d56722c
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-06-09 12:30:23 -0700

    initial commit

---
+++ lib/twitter_many_to_many.rb
@@ -0,0 +1,6 @@
+require "twitter_many_to_many/version"
+
+module TwitterManyToMany
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/twitter_many_to_many_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TwitterManyToMany do
+  it "has a version number" do
+    expect(TwitterManyToMany::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
